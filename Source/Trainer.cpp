1f35b0014ca3cac8a85bde3c61cfebe252a75d98
jbzdarkid <jbzdarkid@gmail.com>
2020-05-30 22:02:11 -0700

    Update doors practice so that panels turn off

--- Source/Trainer.cpp
+++ Source/Trainer.cpp
@@ -39,19 +39,13 @@ std::unique_ptr<Trainer> Trainer::Create(const std::shared_ptr<Memory>& memory) 
         trainer->_solvedTargetOffset = *(int*)&data[index + 0x07];
     });
 
-    // This one is if you haven't run doors injection
     memory->AddSigScan({0x84, 0xC0, 0x74, 0x11, 0x0F, 0x2F, 0xBF}, [&](__int64 offset, int index, const std::vector<byte>& data) {
-        trainer->_doorClose = offset + index + 0x04;
+        trainer->_doorClose = offset + index + 0x0B;
     });
 
-    // And this one is if you have run doors injection
-    memory->AddSigScan({0x84, 0xC0, 0x74, 0x11, 0x3A, 0x87}, [&](__int64 offset, int index, const std::vector<byte>& data) {
-        trainer->_doorClose = offset + index + 0x04;
-    });
-
-    // Improve this? Something awkward is happening if you solve while the doors are closing.
-    memory->AddSigScan({0x76, 0x18, 0x48, 0x8B, 0xCF, 0x89, 0x9F}, [&](__int64 offset, int index, const std::vector<byte>& data) {
-        trainer->_powerOn = offset + index + 0x37;
+    // This sigscan is rather awkward, it's not pointing to the start of a line, because I modify that line.
+    memory->AddSigScan({0x18, 0x48, 0x8B, 0xCF, 0x89, 0x9F}, [&](__int64 offset, int index, const std::vector<byte>& data) {
+        trainer->_powerOn = offset + index - 0x05;
     });
 
     memory->AddSigScan({0x48, 0x89, 0x58, 0x08, 0x48, 0x89, 0x70, 0x10, 0x48, 0x89, 0x78, 0x18, 0x48, 0x8B, 0x3D}, [&](__int64 offset, int index, const std::vector<byte>& data) {
@@ -105,7 +99,6 @@ std::unique_ptr<Trainer> Trainer::Create(const std::shared_ptr<Memory>& memory) 
     trainer->_memory = memory;
 
     size_t numFailedScans = memory->ExecuteSigScans();
-    numFailedScans -= 1; // One of the two _doorClose scans will always fail.
     if (trainer->_globals && trainer->_globals == 0x5B28C0) numFailedScans -= 1; // FOV scan is expected to fail on older versions.
     if (numFailedScans != 0) return nullptr; // Sigscans failed, we'll try again later.
 
@@ -357,7 +350,7 @@ bool Trainer::GetConsoleOpen() {
 
 bool Trainer::GetRandomDoorsPractice() {
     MEMORY_TRY
-        return _memory->ReadData<byte>({_doorOpen}, 1)[0] == 0xEB;
+        return _memory->ReadData<byte>({_doorOpen}, 1)[0] == 0x90;
     MEMORY_CATCH(return false)
 }
 
@@ -469,43 +462,33 @@ void Trainer::SetRandomDoorsPractice(bool enable) {
     MEMORY_CATCH(return)
 
     // If the injection state matches the enable request, no futher action is needed.
-        if (enable == GetRandomDoorsPractice()) return;
+    if (enable == GetRandomDoorsPractice()) return;
 
     MEMORY_TRY
         if (enable) {
-            // When the panel opens, regardless of whether the panel is solved, reset it and power it on.
-            _memory->WriteData<byte>({_doorOpen}, {0xEB, 0x08});
-            // When the panel closes, if the puzzle is solved, turn it off
-            _memory->WriteData<byte>({_doorClose}, {
-                0x3A, 0x87, 0x00, 0x00, 0x00, 0x00, // cmp esi, dword ptr ds:[rdi + <offset>]
-                0x90, // nop
-                0x77 // ja
-                });
-            _memory->WriteData<int>({_doorClose + 0x02}, {hasEverBeenSolvedOffset});
-            // When the panel powers on, mark it as having never been solved
-            _memory->WriteData<byte>({_powerOn}, {
+            // When the panel closes, always turn it off
+            _memory->WriteData<byte>({_doorClose}, {0x90, 0x90});
+            // When the panel opens, always reset it, and power it on.
+            _memory->WriteData<byte>({_doorOpen}, {0x90, 0x90});
+            // When the panel powers on, only randomize if it's solved
+            _memory->WriteData<int>({_powerOn}, {_solvedTargetOffset});
+            _memory->WriteData<byte>({_powerOn + 0x04}, {0x77, 0x18});
+
+            // When the panel powers on, mark it as not solved
+            _memory->WriteData<byte>({_powerOn + 0x3B}, {
                 0x48, 0x63, 0x00, // movsxd rax, dword ptr [rax]
-                0xC6, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, // mov byte ptr ds:[rdi + <offset>], 0x00
-                0x66, 0x90, // nop
-                });
-            _memory->WriteData<int>({_powerOn + 0x05}, {hasEverBeenSolvedOffset});
+                0x66, 0xC7, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov word ptr ds:[rdi + <offset>], 0x00
+            });
+            // Note: We're writing to this register (which is a float!) as a word, since we don't have enough instruction space.
+            // This float should only ever be set to 0x3F800000 (1.0f), though -- so we can just write to the high bits.
+            _memory->WriteData<int>({_powerOn + 0x41}, {_solvedTargetOffset + 2});
         } else {
-            // When the panel opens, if it is not solved, reset it and power it on
-            _memory->WriteData<byte>({_doorOpen}, {0x76, 0x08});
             // When the panel closes, if the puzzle has been solved, turn it off
-            _memory->WriteData<byte>({_doorClose}, {
-                0x0F, 0x2F, 0xBF, 0x00, 0x00, 0x00, 0x00, // comiss xmm7, dword ptr ds:[rdi + <offset>]
-                0x76 // jbe
-                });
-            _memory->WriteData<int>({_doorClose + 0x03}, {_solvedTargetOffset});
-            // When the panel powers on, do not modify its previously-solved state
-            _memory->WriteData<byte>({_powerOn}, {
-                0x48, 0x85, 0xC0, // test rax, rax
-                0x74, 0x5C, // je +0x5C
-                0x48, 0x63, 0x00, // movsxd rax, dword ptr [rax]
-                0x85, 0xC0, // test eax, eax
-                0x74, 0x55, // je +0x55
-                });
+            _memory->WriteData<byte>({_doorClose}, {0x76, 0x08});
+            // When the panel opens, if the puzzle has not been solved, reset it and power it on
+            _memory->WriteData<byte>({_doorOpen}, {0x76, 0x10});
+            // When the panel powers on, always randomize it
+            _memory->WriteData<byte>({_powerOn + 0x04}, {0x90, 0x90});
         }
     MEMORY_CATCH(return)
 }
