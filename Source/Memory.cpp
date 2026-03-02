b6190121efa1fea3dd02556768190a09a913d3a6
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2026-03-01 22:35:27 -0800

    Slight bugfix

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -146,6 +146,7 @@ void Memory::Heartbeat(HWND window, UINT message) {
         _previousCombatStats = playerCombatStats;
         _computedAddresses.Clear();
         PostMessage(window, message, ProcStatus::Reload, NULL);
+		return;
     }
 
     // Otherwise, business as usual.
